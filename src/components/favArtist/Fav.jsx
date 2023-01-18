065ffeb2f7058865040ce861eccbbca5c6f1d2cb
sohseyedi <sohseyedi@gmail.com>
2023-01-18 09:24:36 +0330

    fav song in done

--- src/components/favArtist/Fav.jsx
+++ src/components/favArtist/Fav.jsx
@@ -1,9 +1,28 @@
 import './Fav.scss'
-import Container from './../../container/Container';
+import Container from "../../container/Container";
+import {useSelector} from "react-redux";
+import Song from "../common/SongCard/Song.jsx";
 
 const Fav = () => {
+
+  const {dataItem} = useSelector((state) => state.music)
+  const filterFavItem = dataItem.filter((i) => i.isFavorite === true);
+
+
   return (
-    <Container>Fav</Container>
+      <Container>
+        <div className="fav container">
+          {
+            filterFavItem.length === 0 ?
+                <div className={"text-center text-white pt-5"}> هیچ آهنگ موردعلاقه ای نداری ...</div>
+                : (
+                    filterFavItem.map((song) => (
+                        <Song key={song.id} song={song}/>
+                    ))
+                )
+          }
+        </div>
+      </Container>
   )
 }
 
